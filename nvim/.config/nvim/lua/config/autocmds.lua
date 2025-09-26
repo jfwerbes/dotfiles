aadb69a8fed14965df18f0c4db4b348dd24c6ed8
brutus <jovanfarris@hotmail.com>
2025-09-26 10:46:32 -0700

    autoformat godot script files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Autoformat godotscript files to follow convention
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.gd",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf, lsp_fallback = true })
+  end,
+})
