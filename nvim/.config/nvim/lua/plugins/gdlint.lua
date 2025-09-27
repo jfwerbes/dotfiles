eca347949ef977e78a49c0cd30173ba721da44e0
brutus <jovanfarris@hotmail.com>
2025-09-27 00:02:47 -0700

    fixed gdlint

--- nvim/.config/nvim/lua/plugins/gdlint.lua
+++ nvim/.config/nvim/lua/plugins/gdlint.lua
@@ -1,46 +1,21 @@
+-- .config/nvim/lua/file.lua
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    -- Map the `gdscript` filetype to the custom `gdlint` linter
-    linters_by_ft = {
-      gdscript = { "gdlint" },
-    },
-    linters = {
-      -- Define the custom `gdlint` linter
-      gdlint = {
-        -- `gdtoolkit` is the package that provides `gdlint`.
-        -- If the executable isn't directly `gdlint`, adjust this `cmd` value.
-        cmd = "gdlint",
-        stdin = false,
-        args = { "$FILE" },
-        stream = "stdout",
-        -- The parser is based on `gdlint`'s output format.
-        parser = require("lint.parser").from_errorformat(
-          "%f:%l:%c: %t: %m",
-          -- The skeleton table maps the captured groups to diagnostic fields
-          {
-            file = "%f",
-            lnum = "%l",
-            col = "%c",
-            type = "%t",
-            message = "%m",
-          }
-        ),
-      },
-    },
-  },
-  config = function(_, opts)
-    -- Extend the default linters with your custom ones
-    for name, linter in pairs(opts.linters) do
-      require("lint").linters[name] = linter
-    end
-    require("lint").linters_by_ft = opts.linters_by_ft
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require("lint")
 
-    -- Create an autocommand to run linters on save
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  end,
+      -- Register gdlint as the linter for gdscript
+      lint.linters_by_ft = {
+        gdscript = { "gdlint" },
+      }
+
+      -- Run linting automatically on every write
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
