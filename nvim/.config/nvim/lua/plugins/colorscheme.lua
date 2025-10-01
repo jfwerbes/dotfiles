ded89727bc0c617b2e341958b93ed88fb5d88c53
jovan <jovanfarris@hotmail.com>
2025-09-30 19:55:09 -0700

    made lazyvim transparent, added gruvbox color scheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,17 @@
-return {}
---return {
---  {
---    "folke/tokyonight.nvim",
---    opts = {
---      transparent = true,
---      styles = {
---        sidebars = "transparent",
---        floats = "transparent",
---      },
---    },
---  },
---}
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
