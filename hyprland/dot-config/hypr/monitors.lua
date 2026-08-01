d3595212e250a905be232dc37b4364ab0030bd5b
jovan <jovanfarris@hotmail.com>
2026-08-01 12:55:38 -0700

    hyprland fixed bugs, these layerrules no longer needed

--- hyprland/dot-config/hypr/monitors.lua
+++ hyprland/dot-config/hypr/monitors.lua
@@ -9,12 +9,3 @@ hl.monitor({
 	position = "auto",
 	scale = "auto",
 })
-
--- Custom waybar rules to prevent flickering
---layerrule = blur on, match:class waybar
---layerrule = ignore_alpha 0, match:class waybar
---
---layerrule = blur on, match:class dunst
---layerrule = ignore_alpha 0, match:class dunst
---
---layerrule = blur off, match:class hyprshot
